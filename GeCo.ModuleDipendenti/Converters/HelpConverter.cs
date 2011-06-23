04d838a45e37dd783f31995df237db6bcc2adafd
salem_84 <salem84@hotmail.it>
2011-06-23 12:24:49 +0200

    modifica button style

---
+++ GeCo.ModuleDipendenti/Converters/HelpConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+using GeCo.Infrastructure;
+using GeCo.ModuleDipendenti.ViewModels;
+
+namespace GeCo.Converters
+{
+    public class HelpConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var activeWorkspace = IoC.GetActiveWorkspace<DipendentiWorkspaceContainerVM>();
+            string idActiveWorkspace = activeWorkspace.IdWorkspace;
+            var panel = (HelpPanel) Application.Current.Resources[idActiveWorkspace];
+
+            switch(value.ToString())
+            {
+                case "Titolo":
+                    return panel.Titolo;
+                case "Testo":
+                    return panel.Testo;
+            }
+
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GeCo.ModuleDipendenti/ViewModels/ModuleHelpVM.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GalaSoft.MvvmLight;
+using GeCo.Infrastructure;
+using Microsoft.Practices.Prism.Events;
+using GeCo.Infrastructure.Events;
+using System.Windows;
+
+namespace GeCo.ModuleDipendenti.ViewModels
+{
+    public class ModuleHelpVM : ViewModelBase
+    {
+        private string _titolo;
+        public string Titolo
+        {
+            get { return _titolo; }
+            set
+            {
+                if (_titolo != value)
+                {
+                    _titolo = value;
+                    RaisePropertyChanged("Titolo");
+                }
+            }
+        }
+
+        private string _testo;
+        public string Testo
+        {
+            get { return _testo; }
+            set
+            {
+                if (_testo != value)
+                {
+                    _testo = value;
+                    RaisePropertyChanged("Testo");
+                }
+            }
+        }
+        
+        
+        public ModuleHelpVM()
+        {
+            var eventAggregator = IoC.Get<IEventAggregator>();
+            var changeWorkspaceEvent = eventAggregator.GetEvent<ChangeWorkspaceEvent>();
+            changeWorkspaceEvent.Subscribe(OnChangeWorkspace, ThreadOption.UIThread);
+        }
+
+
+        public void OnChangeWorkspace(ChangeWorkspaceEvent e)
+        {
+            string idActiveWorkspace = e.Workspace.IdWorkspace;
+            if (Application.Current.Resources.Contains(idActiveWorkspace))
+            {
+                var panel = (HelpPanel)Application.Current.Resources[idActiveWorkspace];
+                Titolo = panel.Titolo;
+                Testo = panel.Testo;
+            }
+            else
+            {
+                Titolo = Testo = "";
+            }
+            
+        }
+    }
+
+    
+}
